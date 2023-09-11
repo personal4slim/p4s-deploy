23cad5dab81671025302a6136a2cc19d9132e345
personal4slim <129345989+personal4slim@users.noreply.github.com>
2023-09-11 12:00:29 -0400

    Update 06-aks-cluster.tf

--- smalldeploy/06-aks-cluster.tf
+++ smalldeploy/06-aks-cluster.tf
@@ -42,7 +42,7 @@ resource "azurerm_kubernetes_cluster_node_pool" "win231" {
   name                  = "win231"
   kubernetes_cluster_id = azurerm_kubernetes_cluster.aks_cluster.id
   vm_size               = "Standard_DS2_v2"
-  os_type               = "linux"
+  os_type               = "Linux"
   os_disk_size_gb       = 35
   mode                  = "User"
 
