c1759a97740ca57dff7a1a89abed35cbf15d2e80
personal4slim <129345989+personal4slim@users.noreply.github.com>
2023-09-12 10:22:47 -0400

    Update 06-aks-cluster.tf

--- smalldeploy/06-aks-cluster.tf
+++ smalldeploy/06-aks-cluster.tf
@@ -1,16 +1,36 @@
 resource "azurerm_kubernetes_cluster" "aks_cluster" {
-  name                = "my-aks-cluster"
+  name                = var.cluster_name
   location            = azurerm_resource_group.aks_rg.location
   resource_group_name = azurerm_resource_group.aks_rg.name
-  dns_prefix          = "myaksdns"
+  dns_prefix          = var.dns_prefix
+  kubernetes_version  = var.kubernetes_version
 
   default_node_pool {
     name       = "default"
-    node_count = 1
-    vm_size    = "Standard_DS2_v2"
+    node_count = var.node_count
+    vm_size    = var.vm_size
   }
 
   identity {
     type = "SystemAssigned"
   }
+
+  addon_profile {
+    aci_connector_linux {
+      enabled = false
+    }
+    kube_dashboard {
+      enabled = true
+    }
+  }
+
+  network_profile {
+    network_plugin = "azure"
+  }
+
+  role_based_access_control {
+    enabled = true
+  }
+
+  tags = var.tags
 }
