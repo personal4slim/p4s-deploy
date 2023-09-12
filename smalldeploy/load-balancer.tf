622043f093baf93b8b76969d095d5f64fee02152
personal4slim <129345989+personal4slim@users.noreply.github.com>
2023-09-12 08:51:15 -0400

    Update load-balancer.tf

--- smalldeploy/load-balancer.tf
+++ smalldeploy/load-balancer.tf
@@ -1,28 +1,3 @@
-# Define a managed public IP address for dev environment
-resource "azurerm_public_ip" "dev" {
-  name                = "my-app-service-dev-public-ip"
-  location            = azurerm_resource_group.aks_rg.location
-  resource_group_name = azurerm_resource_group.aks_rg.name
-  allocation_method   = "Dynamic"
-}
-
-# Define a managed public IP address for test environment
-resource "azurerm_public_ip" "test" {
-  name                = "my-app-service-test-public-ip"
-  location            = azurerm_resource_group.aks_rg.location
-  resource_group_name = azurerm_resource_group.aks_rg.name
-  allocation_method   = "Dynamic"
-}
-
-# Define a managed public IP address for prod environment
-resource "azurerm_public_ip" "prod" {
-  name                = "my-app-service-prod-public-ip"
-  location            = azurerm_resource_group.aks_rg.location
-  resource_group_name = azurerm_resource_group.aks_rg.name
-  allocation_method   = "Dynamic"
-}
-
-# Define Load Balancer resources for dev, test, and prod environments
 resource "azurerm_lb" "dev" {
   name                = "my-app-service-dev-lb"
   location            = azurerm_resource_group.aks_rg.location
@@ -43,19 +18,8 @@ resource "azurerm_lb" "dev" {
     request_path = "/"
     port         = 80
   }
-
-  load_balancing_rule {
-    name                       = "webRule"
-    frontend_ip_configuration_id = azurerm_lb.dev.frontend_ip_configuration[0].id
-    backend_address_pool_id      = azurerm_lb.dev.backend_address_pool[0].id
-    probe_id                    = azurerm_lb.dev.probe[0].id
-    protocol                    = "Tcp"
-    frontend_port               = 80
-    backend_port                = 80
-  }
 }
 
-# Define Load Balancer resources for test environment (similar to dev)
 resource "azurerm_lb" "test" {
   name                = "my-app-service-test-lb"
   location            = azurerm_resource_group.aks_rg.location
@@ -76,19 +40,8 @@ resource "azurerm_lb" "test" {
     request_path = "/"
     port         = 80
   }
-
-  load_balancing_rule {
-    name                       = "webRule"
-    frontend_ip_configuration_id = azurerm_lb.test.frontend_ip_configuration[0].id
-    backend_address_pool_id      = azurerm_lb.test.backend_address_pool[0].id
-    probe_id                    = azurerm_lb.test.probe[0].id
-    protocol                    = "Tcp"
-    frontend_port               = 80
-    backend_port                = 80
-  }
 }
 
-# Define Load Balancer resources for prod environment (similar to dev and test)
 resource "azurerm_lb" "prod" {
   name                = "my-app-service-prod-lb"
   location            = azurerm_resource_group.aks_rg.location
@@ -109,14 +62,37 @@ resource "azurerm_lb" "prod" {
     request_path = "/"
     port         = 80
   }
+}
 
-  load_balancing_rule {
-    name                       = "webRule"
-    frontend_ip_configuration_id = azurerm_lb.prod.frontend_ip_configuration[0].id
-    backend_address_pool_id      = azurerm_lb.prod.backend_address_pool[0].id
-    probe_id                    = azurerm_lb.prod.probe[0].id
-    protocol                    = "Tcp"
-    frontend_port               = 80
-    backend_port                = 80
-  }
+resource "azurerm_lb_rule" "dev" {
+  name                      = "webRule"
+  resource_group_name       = azurerm_resource_group.aks_rg.name
+  loadbalancer_id           = azurerm_lb.dev.id
+  frontend_ip_configuration = azurerm_lb.dev.frontend_ip_configuration[0].name
+  frontend_port             = 80
+  backend_address_pool_id    = azurerm_lb.dev.backend_address_pool[0].id
+  probe_id                  = azurerm_lb.dev.probe[0].id
+  protocol                  = "Tcp"
+}
+
+resource "azurerm_lb_rule" "test" {
+  name                      = "webRule"
+  resource_group_name       = azurerm_resource_group.aks_rg.name
+  loadbalancer_id           = azurerm_lb.test.id
+  frontend_ip_configuration = azurerm_lb.test.frontend_ip_configuration[0].name
+  frontend_port             = 80
+  backend_address_pool_id    = azurerm_lb.test.backend_address_pool[0].id
+  probe_id                  = azurerm_lb.test.probe[0].id
+  protocol                  = "Tcp"
+}
+
+resource "azurerm_lb_rule" "prod" {
+  name                      = "webRule"
+  resource_group_name       = azurerm_resource_group.aks_rg.name
+  loadbalancer_id           = azurerm_lb.prod.id
+  frontend_ip_configuration = azurerm_lb.prod.frontend_ip_configuration[0].name
+  frontend_port             = 80
+  backend_address_pool_id    = azurerm_lb.prod.backend_address_pool[0].id
+  probe_id                  = azurerm_lb.prod.probe[0].id
+  protocol                  = "Tcp"
 }
