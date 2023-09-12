33b7b8b3495bce10eb5bbe478c59e9e71d09abd1
personal4slim <129345989+personal4slim@users.noreply.github.com>
2023-09-12 10:20:17 -0400

    Update 06-aks-cluster.tf

--- smalldeploy/06-aks-cluster.tf
+++ smalldeploy/06-aks-cluster.tf
@@ -2,8 +2,7 @@ resource "azurerm_kubernetes_cluster" "aks_cluster" {
   name                = "my-aks-cluster"
   location            = azurerm_resource_group.aks_rg.location
   resource_group_name = azurerm_resource_group.aks_rg.name
-  dns_prefix          = "myakscluster"
-  kubernetes_version  = "1.21.2"
+  dns_prefix          = "myaksdns"
 
   default_node_pool {
     name       = "default"
@@ -14,35 +13,4 @@ resource "azurerm_kubernetes_cluster" "aks_cluster" {
   identity {
     type = "SystemAssigned"
   }
-
-  network_profile {
-    network_plugin = "azure"
-    load_balancer_sku = "Standard"
-  }
-
-  addon_profile {
-    kube_dashboard {
-      enabled = true
-    }
-
-    oms_agent {
-      enabled = true
-      log_analytics_workspace_id = azurerm_log_analytics_workspace.example.id
-    }
-  }
-
-  role_based_access_control {
-    enabled = true
-  }
-
-  tags = {
-    Environment = "Dev"
-  }
 }
-
-data "azurerm_log_analytics_workspace" "example" {
-  name                = "example"
-  resource_group_name = azurerm_resource_group.aks_rg.name
-}
-
-# Other resources related to your AKS cluster
