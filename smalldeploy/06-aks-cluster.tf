d1f1bd9cc8161a3ae74a9cdbcede8cede88b9bba
personal4slim <129345989+personal4slim@users.noreply.github.com>
2023-09-12 09:58:55 -0400

    Update 06-aks-cluster.tf

--- smalldeploy/06-aks-cluster.tf
+++ smalldeploy/06-aks-cluster.tf
@@ -1,47 +1,65 @@
+# Define the Azure Kubernetes Cluster
 resource "azurerm_kubernetes_cluster" "aks_cluster" {
-  name                = "${azurerm_resource_group.aks_rg.name}-cluster"
+  name                = "my-aks-cluster"
   location            = azurerm_resource_group.aks_rg.location
   resource_group_name = azurerm_resource_group.aks_rg.name
-  dns_prefix          = "${azurerm_resource_group.aks_rg.name}-cluster"
-  kubernetes_version  = data.azurerm_kubernetes_service_versions.current.latest_version
-  node_resource_group = "${azurerm_resource_group.aks_rg.name}-nrg"
+  dns_prefix          = "myakscluster"
 
   default_node_pool {
-    name                = "p4spool"
-    node_count          = 1
-    vm_size             = "Standard_D2_v2"
-    orchestrator_version = data.azurerm_kubernetes_service_versions.current.latest_version
-    enable_auto_scaling = true
-    max_count           = 3
-    min_count           = 1
-    os_disk_size_gb     = 30
-    type                = "VirtualMachineScaleSets"
-
-    load_balancer_sku = "Standard"  # You can adjust this based on your requirements
-    outbound_type     = "loadBalancer"
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
   }
 
   identity {
     type = "SystemAssigned"
   }
 
-  # Role Based Access Control
-  azure_active_directory_role_based_access_control {
-    managed              = true
-    admin_group_object_ids = ["002fae4f-5ba5-47f1-800a-70d428de60b7"]
+  network_profile {
+    network_plugin = "azure"
+    load_balancer_sku = "Standard"
   }
 
-  network_profile {
-    network_plugin = "azure"  # Use the "azure" network plugin for Windows agent pool
-
-    load_balancer_profile {
-      managed_outbound_ip_count = 1  # You can adjust this based on your requirements
-      outbound_ip_prefix_ids    = [azurerm_public_ip.dev.id, azurerm_public_ip.test.id, azurerm_public_ip.prod.id]
+  addon_profile {
+    oms_agent {
+      enabled                    = false
     }
   }
 
-  windows_profile {
-    admin_username = "personal4slim"
-    admin_password = "Oluwaseun_101#"
+  role_based_access_control {
+    enabled = true
   }
+
+  tags = {
+    environment = "Dev"
+  }
+
+  # Reference to the outbound IP prefixes from the load balancers
+  outbound_ip_prefix_ids = [
+    azurerm_public_ip.dev.id,
+    azurerm_public_ip.test.id,
+    azurerm_public_ip.prod.id
+  ]
+}
+
+# Define managed public IP addresses for dev, test, and prod environments
+resource "azurerm_public_ip" "dev" {
+  name                = "my-app-service-dev-public-ip"
+  location            = azurerm_resource_group.aks_rg.location
+  resource_group_name = azurerm_resource_group.aks_rg.name
+  allocation_method   = "Dynamic"
+}
+
+resource "azurerm_public_ip" "test" {
+  name                = "my-app-service-test-public-ip"
+  location            = azurerm_resource_group.aks_rg.location
+  resource_group_name = azurerm_resource_group.aks_rg.name
+  allocation_method   = "Dynamic"
+}
+
+resource "azurerm_public_ip" "prod" {
+  name                = "my-app-service-prod-public-ip"
+  location            = azurerm_resource_group.aks_rg.location
+  resource_group_name = azurerm_resource_group.aks_rg.name
+  allocation_method   = "Dynamic"
 }
