47df9615a1817ee01dbb4fd7c9184387a991ad1d
personal4slim <129345989+personal4slim@users.noreply.github.com>
2023-09-12 10:37:48 -0400

    Update load-balancer.tf

--- smalldeploy/load-balancer.tf
+++ smalldeploy/load-balancer.tf
@@ -1,101 +1,113 @@
 resource "azurerm_lb" "dev" {
-  name                = "my-app-service-dev-lb"
-  location            = azurerm_resource_group.aks_rg.location
-  resource_group_name = azurerm_resource_group.aks_rg.name
-  sku                 = "Basic"
+  name                = "example-lb-dev"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  sku                 = "Standard"
 
   frontend_ip_configuration {
     name                 = "PublicIPAddress"
     public_ip_address_id = azurerm_public_ip.dev.id
   }
 
+  tags = var.tags
+
   backend_address_pool {
-    name = "backendPool1"
+    name = "backendPool"
   }
 
   probe {
-    name         = "httpProbe"
-    protocol     = "Http"
-    request_path = "/"
-    port         = 80
+    name                      = "httpProbe"
+    protocol                  = "HTTP"
+    request_path              = "/"
+    port                      = 80
+    interval_in_seconds       = 5
+    number_of_probes          = 2
+  }
+
+  load_balancing_rule {
+    name                       = "httpRule"
+    frontend_ip_configuration  = azurerm_lb.dev.frontend_ip_configuration[0].name
+    backend_address_pool       = azurerm_lb.dev.backend_address_pool[0].name
+    probe                      = azurerm_lb.dev.probe[0].name
+    protocol                   = "Tcp"
+    frontend_port              = 80
+    backend_port               = 80
+    enable_floating_ip         = false
   }
 }
 
 resource "azurerm_lb" "test" {
-  name                = "my-app-service-test-lb"
-  location            = azurerm_resource_group.aks_rg.location
-  resource_group_name = azurerm_resource_group.aks_rg.name
-  sku                 = "Basic"
+  name                = "example-lb-test"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  sku                 = "Standard"
 
   frontend_ip_configuration {
     name                 = "PublicIPAddress"
     public_ip_address_id = azurerm_public_ip.test.id
   }
 
+  tags = var.tags
+
   backend_address_pool {
-    name = "backendPool1"
+    name = "backendPool"
   }
 
   probe {
-    name         = "httpProbe"
-    protocol     = "Http"
-    request_path = "/"
-    port         = 80
+    name                      = "httpProbe"
+    protocol                  = "HTTP"
+    request_path              = "/"
+    port                      = 80
+    interval_in_seconds       = 5
+    number_of_probes          = 2
+  }
+
+  load_balancing_rule {
+    name                       = "httpRule"
+    frontend_ip_configuration  = azurerm_lb.test.frontend_ip_configuration[0].name
+    backend_address_pool       = azurerm_lb.test.backend_address_pool[0].name
+    probe                      = azurerm_lb.test.probe[0].name
+    protocol                   = "Tcp"
+    frontend_port              = 80
+    backend_port               = 80
+    enable_floating_ip         = false
   }
 }
 
 resource "azurerm_lb" "prod" {
-  name                = "my-app-service-prod-lb"
-  location            = azurerm_resource_group.aks_rg.location
-  resource_group_name = azurerm_resource_group.aks_rg.name
-  sku                 = "Basic"
+  name                = "example-lb-prod"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  sku                 = "Standard"
 
   frontend_ip_configuration {
     name                 = "PublicIPAddress"
     public_ip_address_id = azurerm_public_ip.prod.id
   }
 
+  tags = var.tags
+
   backend_address_pool {
-    name = "backendPool1"
+    name = "backendPool"
   }
 
   probe {
-    name         = "httpProbe"
-    protocol     = "Http"
-    request_path = "/"
-    port         = 80
+    name                      = "httpProbe"
+    protocol                  = "HTTP"
+    request_path              = "/"
+    port                      = 80
+    interval_in_seconds       = 5
+    number_of_probes          = 2
   }
-}
 
-resource "azurerm_lb_rule" "dev" {
-  name                      = "webRule"
-  resource_group_name       = azurerm_resource_group.aks_rg.name
-  loadbalancer_id           = azurerm_lb.dev.id
-  frontend_ip_configuration = azurerm_lb.dev.frontend_ip_configuration[0].name
-  frontend_port             = 80
-  backend_address_pool_id   = azurerm_lb.dev.backend_address_pool[0].id
-  probe_id                  = azurerm_lb.dev.probe[0].id
-  protocol                  = "Tcp"
-}
-
-resource "azurerm_lb_rule" "test" {
-  name                      = "webRule"
-  resource_group_name       = azurerm_resource_group.aks_rg.name
-  loadbalancer_id           = azurerm_lb.test.id
-  frontend_ip_configuration = azurerm_lb.test.frontend_ip_configuration[0].name
-  frontend_port             = 80
-  backend_address_pool_id   = azurerm_lb.test.backend_address_pool[0].id
-  probe_id                  = azurerm_lb.test.probe[0].id
-  protocol                  = "Tcp"
-}
-
-resource "azurerm_lb_rule" "prod" {
-  name                      = "webRule"
-  resource_group_name       = azurerm_resource_group.aks_rg.name
-  loadbalancer_id           = azurerm_lb.prod.id
-  frontend_ip_configuration = azurerm_lb.prod.frontend_ip_configuration[0].name
-  frontend_port             = 80
-  backend_address_pool_id   = azurerm_lb.prod.backend_address_pool[0].id
-  probe_id                  = azurerm_lb.prod.probe[0].id
-  protocol                  = "Tcp"
+  load_balancing_rule {
+    name                       = "httpRule"
+    frontend_ip_configuration  = azurerm_lb.prod.frontend_ip_configuration[0].name
+    backend_address_pool       = azurerm_lb.prod.backend_address_pool[0].name
+    probe                      = azurerm_lb.prod.probe[0].name
+    protocol                   = "Tcp"
+    frontend_port              = 80
+    backend_port               = 80
+    enable_floating_ip         = false
+  }
 }
