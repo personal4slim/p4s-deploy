0b31a2b08e9af76dfd0aa721bf50234ecd7f49da
personal4slim <129345989+personal4slim@users.noreply.github.com>
2023-09-12 07:02:36 -0400

    Update 06-aks-cluster.tf

--- smalldeploy/06-aks-cluster.tf
+++ smalldeploy/06-aks-cluster.tf
@@ -3,7 +3,7 @@ resource "azurerm_kubernetes_cluster" "aks_cluster" {
   location            = azurerm_resource_group.aks_rg.location
   resource_group_name = azurerm_resource_group.aks_rg.name
   dns_prefix          = "${azurerm_resource_group.aks_rg.name}-cluster"
-  kubernetes_version  = data.azurerm_kubernetes_service_versions.current.latest_version
+  kubernetes_version = data.azurerm_kubernetes_service_versions.current.latest_version
   node_resource_group = "${azurerm_resource_group.aks_rg.name}-nrg"
 
   default_node_pool {
@@ -36,30 +36,57 @@ resource "azurerm_kubernetes_cluster" "aks_cluster" {
     admin_username = "personal4slim"
     admin_password = "Oluwaseun_101#"
   }
+}
 
-  node_pool {
-    name                = "win231"
-    vm_size             = "Standard_DS2_v2"
-    os_type             = "Linux"
-    os_disk_size_gb     = 35
-    enable_auto_scaling = true
-    max_count           = 3
-    min_count           = 1
+resource "azurerm_kubernetes_cluster_node_pool" "win231" {
+  name                  = "win231"
+  kubernetes_cluster_id = azurerm_kubernetes_cluster.aks_cluster.id
+  vm_size               = "Standard_DS2_v2"
+  os_type               = "Linux"
+  os_disk_size_gb       = 35
+  mode                  = "User"
 
-    node_labels = {
-      "nodepool-type" = "user"
-      "environment"   = "dev"
-      "nodepoolos"    = "linux"
-      "app"           = "system-apps"
-    }
+  enable_auto_scaling = true
+  max_count           = 3
+  min_count           = 1
 
-    tags = {
-      "nodepool-type" = "user"
-      "environment"   = "dev"
-      "nodepools"     = "linux"
-      "app"           = "system-apps"
-    }
-
-    priority = "Regular"
+  node_labels = {
+    "nodepool-type" = "user"
+    "environment"   = "dev"
+    "nodepoolos"    = "linux"
+    "app"           = "system-apps"
   }
+
+  tags = {
+    "nodepool-type" = "user"
+    "environment"   = "dev"
+    "nodepools"     = "linux"
+    "app"           = "system-apps"
+  }
+
+  priority = "Regular"
+}
+
+# Define a managed public IP address for dev environment
+resource "azurerm_public_ip" "dev" {
+  name                = "my-app-service-dev-public-ip"
+  location            = azurerm_resource_group.aks_rg.location
+  resource_group_name = azurerm_resource_group.aks_rg.name
+  allocation_method   = "Dynamic"
+}
+
+# Define a managed public IP address for test environment
+resource "azurerm_public_ip" "test" {
+  name                = "my-app-service-test-public-ip"
+  location            = azurerm_resource_group.aks_rg.location
+  resource_group_name = azurerm_resource_group.aks_rg.name
+  allocation_method   = "Dynamic"
+}
+
+# Define a managed public IP address for prod environment
+resource "azurerm_public_ip" "prod" {
+  name                = "my-app-service-prod-public-ip"
+  location            = azurerm_resource_group.aks_rg.location
+  resource_group_name = azurerm_resource_group.aks_rg.name
+  allocation_method   = "Dynamic"
 }
